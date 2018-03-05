d519964eed9a49188e2dcb052436c845397ffa02
Jaden Young <jaden.young44@gmail.com>
2018-03-04 20:56:26 -0600

    Initial skeleton generated by Spring Initalizr

---
+++ src/main/java/com/github/eiffelcommunity/eiffeljiraplugin/EiffelJiraPluginApplication.java
@@ -0,0 +1,12 @@
+package com.github.eiffelcommunity.eiffeljiraplugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EiffelJiraPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EiffelJiraPluginApplication.class, args);
+	}
+}
